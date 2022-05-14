d81d54823dc6a6ade7ca10d3024a0ab609586d37
Eman Bassem <82765578+emann13@users.noreply.github.com>
2022-05-14 15:26:08 +0200

    Add files via upload

--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -1,12 +1,27 @@
 import { Injectable } from '@angular/core';
 import {AngularFireAuth} from '@angular/fire/compat/auth'
+import { HomeComponent } from '../home/home.component';
+import {AngularFirestore} from '@angular/fire/compat/firestore'
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'any',
 })
 export class FirebaseService {
 
   isLoggedIn = false
-  constructor(public firebaseAuth : AngularFireAuth) { }
+  auth = getAuth();
+  user = this.auth.currentUser;
+
+
+ 
+  constructor(public firebaseAuth : AngularFireAuth,private _AngularFirestore :AngularFirestore) { }
+  idd(){
+  if (this.user != null) {
+    const uid = this.user.uid;
+    this._AngularFirestore.collection('signUp').add(uid);
+    window.alert(uid);
+  }}
   async signin(email: string, password : string){
     await this.firebaseAuth.signInWithEmailAndPassword(email,password)
     .then(res=>{
@@ -19,10 +34,13 @@ export class FirebaseService {
     .then(res=>{
       this.isLoggedIn = true
       localStorage.setItem('user',JSON.stringify(res.user))
+      this.idd();
+        
     })
   }
   logout(){
     this.firebaseAuth.signOut()
     localStorage.removeItem('user')
   }
+  
 }
