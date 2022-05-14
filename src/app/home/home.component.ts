01a562273ef9354bfa01136ec5ca8c42e05365b3
Eman Bassem <82765578+emann13@users.noreply.github.com>
2022-05-14 15:25:24 +0200

    Add files via upload

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Router } from '@angular/router';
 import { FirebaseService } from '../services/firebase.service';
 
 @Component({
@@ -6,16 +8,54 @@ import { FirebaseService } from '../services/firebase.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class  HomeComponent implements OnInit{
+  private readonly newProperty = 'home-root';
 
-  @Output() isLogout = new EventEmitter<void>()
-  constructor(public firebaseService: FirebaseService) { }
-
-  ngOnInit(): void {
+  title = 'ISeeU';
+  isSignedIn = false
+  
+  constructor(private router: Router,  public firebaseService : FirebaseService){}
+  
+  goToHome() {
+    this.router.navigate(['vitals']);
   }
-  logout(){
-    this.firebaseService.logout()
-    this.isLogout.emit()
+  ngOnInit(){
+    if(localStorage.getItem('user')!== null)
+    this.isSignedIn= true
+    else
+    this.isSignedIn = false
+  }
+  async onSignup(email:string,password:string){
+    await this.firebaseService.signup(email,password)
+    if(this.firebaseService.isLoggedIn)
+   {     this.router.navigate(['vitals']);
+
+    this.isSignedIn = true;  }
+    
   }
 
+  async onSignin(email:string,password:string){
+    await this.firebaseService.signin(email,password)
+    if(this.firebaseService.isLoggedIn)
+   { this.router.navigate(['vitals']);
+    window.alert("welcome!")
+    this.isSignedIn = true;
+  
+  }
+  }
+  handleLogout(){
+    this.isSignedIn = false
+
+  }
+  
+  // writeUserData(userId : number , name, email, imageUrl) {
+  //   firebase
+  //     .database()
+  //     .ref('users/' + userId)
+  //     .set({
+  //       username: name,
+  //       email: email,
+  //       profile_picture: imageUrl,
+  //     });
+  // }
 }
