6b90c4e2505fe8e37feabea4e79acc14d82d3ea1
Eman Bassem <82765578+emann13@users.noreply.github.com>
2022-05-19 01:22:19 +0200

    Add files via upload

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,44 +1,56 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Router } from '@angular/router';
-import { FirebaseService } from '../services/firebase.service';
+import { User } from '../module/user';
+import { FirebaseService } from '../service/firebase.service';
+import { DBService } from '../services/db.service';
+// import * as firebase from 'firebase/compat';
+// import { FirebaseService } from '../services/firebase.service';
 
 @Component({
-  selector: 'app-home',
+  selector: 'home-root',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class  HomeComponent implements OnInit{
   private readonly newProperty = 'home-root';
-
+  
   title = 'ISeeU';
   isSignedIn = false
+  user=new User();
   
-  constructor(private router: Router,  public firebaseService : FirebaseService){}
+  constructor(private router: Router,  private db:DBService,private firebaseService:FirebaseService){}
   
   goToHome() {
     this.router.navigate(['vitals']);
   }
   ngOnInit(){
-    if(localStorage.getItem('user')!== null)
-    this.isSignedIn= true
-    else
-    this.isSignedIn = false
+    // if(localStorage.getItem('user')!== null)
+    // this.isSignedIn= true
+    // else
+    // this.isSignedIn = false
   }
   async onSignup(email:string,password:string){
     await this.firebaseService.signup(email,password)
     if(this.firebaseService.isLoggedIn)
-   {     this.router.navigate(['vitals']);
-
+    {          
+       
+     this.router.navigate(['vitals']);
     this.isSignedIn = true;  }
+    this.user.Email=email;
+    this.user.Password=password;
+    this.firebaseService.newdoc(this.user)
+    //   window.alert(data)
+    // })
     
   }
 
   async onSignin(email:string,password:string){
     await this.firebaseService.signin(email,password)
     if(this.firebaseService.isLoggedIn)
-   { this.router.navigate(['vitals']);
-    window.alert("welcome!")
+   {                   
+     this.router.navigate(['vitals']);
+    // window.alert("welcome!")
     this.isSignedIn = true;
   
   }
